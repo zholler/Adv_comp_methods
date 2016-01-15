0881eff09c9cf2a9ec60823f34a0a09bafeb1ab9
zsuzsaholler <holler.zsuzsa@gmail.com>
2016-01-15 11:40:25 +0100

    Final version of genData.R

--- PS1/genData.R
+++ PS1/genData.R
@@ -1,9 +1,14 @@
-N = 1000
-genData = function(N){
+genData = function(N, saveData=TRUE, savePlot=TRUE){
+  #Generates random points in a map (e.g. location of houses for sale) and adds a label whether
+  #the point is in an urban or in a suburban area. Location and size of suburban areas are also 
+  #randomly generated.
   
-  set.seed(9866)
+  set.seed(78696)
+  if (!require("ggplot2")) install.packages("ggplot2"); library(ggplot2)
+  #Max value of latitude and longitude - defines size of the whole area
   latitude_max = 1
   longitude_max = 1
+  #Number ofsuburban areas
   n = 5
   
   genAreas = function(latitude_max = 1, longitude_max = 1, center_size=0.4 , n = 5, ratio = 0.2) {
@@ -47,21 +52,48 @@ genData = function(N){
   }
   suburban_areas = genAreas()
   
+  #Function to compute distace of some points from a center
   dist <- function(points,center) {
     apply(points , 1 , function(x) sum((x - center)^2)^(1/2) )
   }
   
+  #Generating random location data (e.g.: location of houses for sale)
   sample_latitude = runif(N,min=0,max=latitude_max)
   sample_longitude = runif(N, min=0, max=longitude_max)
-  data = cbind(sample_latitude, sample_longitude)
+  data = as.data.frame(cbind(sample_latitude, sample_longitude))
   
+  #Initialize catgory variable - False if house is in urban area, True if in suburban
   suburban = rep(FALSE, dim(data)[1])
+  #Check if house is in suburban area
   for (i in 1:n) {
     suburban = suburban | dist(data,suburban_areas$center_suburban[,i]) < suburban_areas$r_suburban[i]
   }
   
-  plot(data[suburban == F,], pch = 4)
-  points(data[suburban == T,], col="green", pch = 4)
-  points(t(suburban_areas$center_suburban) , col="red", pch = 4)
+  #Add category
+  data=cbind(data,suburban)
+  #Add category labels
+  data$suburban_label = rep("urban",nrow(data))
+  data$suburban_label[data$suburban==TRUE] = "suburban"
   
+  #Save data
+  if (saveData) {
+    write.csv(data,file = "dataset.csv")
+  }
+  
+  #Saving plot
+  if (savePlot) {
+    
+    ggplot(data = data,
+           aes(x = sample_latitude, y = sample_longitude, colour=suburban_label)) +
+      geom_point() +
+      xlab("latitude") +
+      ylab("longitude")  + theme_bw() +
+      scale_color_manual("Category", 
+                         values = c("urban" = "black", "suburban" = "green"))
+    ggsave("dataPlot.pdf")
+  }
+  return(data)
 }
+
+N = 1000
+data = genData(N)
